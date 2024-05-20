ffe85965bbd853cc2e5d79c293cedcf5d68e1dc5
Will Ruggiano <wmruggiano@gmail.com>
2024-05-20 14:32:23 -0600

    feat: re-enable introspection

--- bin/app.ts
+++ bin/app.ts
@@ -18,6 +18,7 @@ const httpServer = http.createServer(app);
 const server = new ApolloServer<Context>({
   resolvers,
   typeDefs,
+  introspection: true,
   plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
 });
 
