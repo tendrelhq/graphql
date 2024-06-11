7f5a245ba7927932717a96cb36fad25e29cd9a44
Will Ruggiano <wmruggiano@gmail.com>
2024-06-11 09:13:31 -0600

    fix: name loader

--- lib/datasources/name.ts
+++ lib/datasources/name.ts
@@ -9,33 +9,30 @@ type Key = { id: string; language_id: string };
 export function makeNameLoader(_: Request) {
   return new Dataloader<Key, Name, string>(
     async keys => {
-      const rows = await sql<Name[]>`
-          SELECT
-              m.languagemasteruuid AS id,
-              COALESCE(tl.systaguuid, ml.systaguuid) AS language_id,
-              COALESCE(t.languagetranslationvalue, m.languagemastersource) AS value
-          FROM public.languagemaster AS m
-          INNER JOIN public.systag AS ml
-              ON m.languagemastersourcelanguagetypeid = ml.systagid
-          LEFT JOIN public.languagetranslations AS t
-              ON m.languagemasterid = t.languagetranslationmasterid
-          LEFT JOIN public.systag AS tl
-              ON t.languagetranslationtypeid = tl.systagid
-          WHERE
-              (m.languagemasteruuid, COALESCE(tl.systaguuid, ml.systaguuid)) IN ${sql(
-                keys.map(k => sql([k.id, k.language_id])),
-              )};
-      `;
-
-      const byId = rows.reduce(
-        (acc, row) => acc.set(`${row.id}:${row.language_id}`, row),
-        new Map<string, Name>(),
-      );
-
-      return keys.map(
-        key =>
-          byId.get(`${key.id}:${key.language_id}`) ??
-          new NotFoundError(`${key.id}:${key.language_id}`, "name"),
+      return await Promise.all(
+        keys.map(async k => {
+          const [name] = await sql<[Name?]>`
+              SELECT
+                  m.languagemasteruuid AS id,
+                  COALESCE(t.languagetranslationvalue, m.languagemastersource) AS value,
+                  COALESCE(tl.systaguuid, ml.systaguuid) AS language_id
+              FROM public.languagemaster AS m
+              INNER JOIN public.systag AS ml
+                  ON m.languagemastersourcelanguagetypeid = ml.systagid
+              LEFT JOIN public.languagetranslations AS t
+                  ON
+                      m.languagemasterid = t.languagetranslationmasterid
+                      AND t.languagetranslationtypeid = (
+                          SELECT systagid
+                          FROM public.systag
+                          WHERE systaguuid = ${k.language_id}
+                      )
+              LEFT JOIN public.systag AS tl
+                  ON t.languagetranslationtypeid = tl.systagid
+              WHERE m.languagemasteruuid = ${k.id};
+          `;
+          return name ?? new NotFoundError(`${k.id}:${k.language_id}`, "name");
+        }),
       );
     },
     {
