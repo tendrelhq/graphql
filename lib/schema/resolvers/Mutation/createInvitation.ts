5568978e8a16a8cb4e0c7149ebf31fd3f5121d03
Will Ruggiano <wmruggiano@gmail.com>
2024-06-14 14:45:27 -0600

    fix: better error message for clerk's duplicate_record error

--- lib/schema/resolvers/Mutation/createInvitation.ts
+++ lib/schema/resolvers/Mutation/createInvitation.ts
@@ -1,6 +1,8 @@
 import { protect } from "@/auth";
 import type { MutationResolvers } from "@/schema";
 import { clerkClient } from "@clerk/clerk-sdk-node";
+import { isClerkAPIResponseError, isClerkRuntimeError } from "@clerk/shared";
+import { GraphQLError } from "graphql";
 
 export const createInvitation: NonNullable<
   MutationResolvers["createInvitation"]
@@ -9,13 +11,26 @@ export const createInvitation: NonNullable<
 
   // FIXME: This is pretty fucked. But I suppose ok for now.
   // Will be fixed when we (hopefully) integrate with Clerk organizations.
-  const i = await clerkClient.invitations.createInvitation({
-    emailAddress: input.email_address,
-    publicMetadata: {
-      needs_sync: "yes",
-      tendrel_id: input.worker_id,
-    },
-  });
+  const i = await clerkClient.invitations
+    .createInvitation({
+      emailAddress: input.email_address,
+      publicMetadata: {
+        needs_sync: "yes",
+        tendrel_id: input.worker_id,
+      },
+    })
+    .catch(e => {
+      if (isClerkAPIResponseError(e)) {
+        const ce = e.errors.at(0);
+        throw new GraphQLError(ce?.longMessage ?? ce?.message ?? e.message, {
+          extensions: {
+            code: ce?.code ?? "BAD_REQUEST",
+          },
+        });
+      }
+
+      throw e;
+    });
 
   return {
     id: i.id,
