db06c6db39576b3b987801867eedbc36de73a4aa
Will Ruggiano <wmruggiano@gmail.com>
2024-06-28 00:49:50 -0700

    [𝘀𝗽𝗿] initial version
    Created using spr 1.3.4

--- lib/schema/resolvers/Mutation/disableLanguage.ts
+++ lib/schema/resolvers/Mutation/disableLanguage.ts
@@ -5,45 +5,47 @@ import { GraphQLError } from "graphql";
 export const disableLanguage: NonNullable<
   MutationResolvers["disableLanguage"]
 > = async (_, { orgId, languageId }, __) => {
-  const check = await sql`
-      SELECT 1
-      FROM public.customer
-      WHERE
-          customeruuid = ${orgId}
-          AND customerlanguagetypeid = (
-              SELECT systagid
-              FROM public.systag
-              WHERE systaguuid = ${languageId}
-          );
-  `;
+  await sql.begin(async sql => {
+    const check = await sql`
+        SELECT 1
+        FROM public.customer
+        WHERE
+            customeruuid = ${orgId}
+            AND customerlanguagetypeid = (
+                SELECT systagid
+                FROM public.systag
+                WHERE systaguuid = ${languageId}
+            );
+    `;
 
-  // Disallow disabling an organization's primary language.
-  if (check.length) {
-    throw new GraphQLError("Cannot disable primary language", {
-      extensions: {
-        code: "BAD_REQUEST",
-        hint: "Try changing your primary language first",
-      },
-    });
-  }
+    // Disallow disabling an organization's primary language.
+    if (check.length) {
+      throw new GraphQLError("Cannot disable primary language", {
+        extensions: {
+          code: "BAD_REQUEST",
+          hint: "Try changing your primary language first",
+        },
+      });
+    }
 
-  await sql`
-      UPDATE public.customerrequestedlanguage
-      SET
-          customerrequestedlanguageenddate = NOW(),
-          customerrequestedlanguagemodifieddate = NOW()
-      WHERE
-          customerrequestedlanguagecustomerid = (
-              SELECT customerid
-              FROM public.customer
-              WHERE customeruuid = ${orgId}
-          )
-          AND customerrequestedlanguagelanguageid = (
-              SELECT systagid
-              FROM public.systag
-              WHERE systaguuid = ${languageId}
-          );
-  `;
+    await sql`
+        UPDATE public.customerrequestedlanguage
+        SET
+            customerrequestedlanguageenddate = NOW(),
+            customerrequestedlanguagemodifieddate = NOW()
+        WHERE
+            customerrequestedlanguagecustomerid = (
+                SELECT customerid
+                FROM public.customer
+                WHERE customeruuid = ${orgId}
+            )
+            AND customerrequestedlanguagelanguageid = (
+                SELECT systagid
+                FROM public.systag
+                WHERE systaguuid = ${languageId}
+            );
+    `;
+  });
 
-  return true;
+  return true; // FIXME: should return an Activated/ActivationState
 };
