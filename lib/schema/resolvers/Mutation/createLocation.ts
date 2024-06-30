bfeb224de4c6c6e74b056e4f9e5cc988199b372d
Will Ruggiano <wmruggiano@gmail.com>
2024-06-30 13:09:40 -0700

    feat: telemetry

--- lib/schema/resolvers/Mutation/createLocation.ts
+++ lib/schema/resolvers/Mutation/createLocation.ts
@@ -1,7 +1,8 @@
 import { sql } from "@/datasources/postgres";
 import type { MutationResolvers } from "@/schema";
 
-export const createLocation: NonNullable<MutationResolvers["createLocation"]> =
-  async (_, { input }, ctx) => {
-    throw new Error("not implemented");
-  };
+export const createLocation: NonNullable<
+  MutationResolvers["createLocation"]
+> = async (_, { input }, ctx) => {
+  throw new Error("not implemented");
+};
