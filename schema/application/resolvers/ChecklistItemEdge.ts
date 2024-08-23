b8854c06eed3cfa695b23504e241f6f2301c24a3
Will Ruggiano <wmruggiano@gmail.com>
2024-08-22 18:23:32 -0700

    more schema refactor

---
+++ schema/application/resolvers/ChecklistItemEdge.ts
@@ -0,0 +1,4 @@
+import type { ChecklistItemEdgeResolvers } from "./../../__generated__/types.generated";
+export const ChecklistItemEdge: ChecklistItemEdgeResolvers = {
+  /* Implement ChecklistItemEdge resolver logic here */
+};
