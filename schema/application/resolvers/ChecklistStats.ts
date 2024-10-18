9ed6ff376258aa54c6c29121c1a73baef08c0571
Will Ruggiano <wmruggiano@gmail.com>
2024-10-17 19:45:00 -0700

    this is more like what i was talking about

--- schema/application/resolvers/ChecklistStats.ts
+++
@@ -1,4 +0,0 @@
-import type { ChecklistStatsResolvers } from "./../../__generated__/types.generated";
-export const ChecklistStats: ChecklistStatsResolvers = {
-  /* Implement ChecklistStats resolver logic here */
-};
