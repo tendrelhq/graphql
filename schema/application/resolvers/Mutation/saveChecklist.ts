7a177d346b604b3288fb4fce610ff38a4d7f801c
Will Ruggiano <wmruggiano@gmail.com>
2024-10-18 15:22:40 -0700

    fix: changing a checklist name does not rerender as expected

--- schema/application/resolvers/Mutation/saveChecklist.ts
+++ schema/application/resolvers/Mutation/saveChecklist.ts
@@ -157,32 +157,49 @@ export const saveChecklist: NonNullable<
                 worktemplatedescriptionid IS NOT null
         `,
       tx`
-        WITH inputs (value, locale) AS (
-            VALUES (
-                ${input.name.value.value},
-                ${input.name.value.locale}
+        WITH
+            inputs AS (
+                SELECT
+                    ${input.name.value.value}::text AS name,
+                    systagid AS locale
+                FROM public.systag
+                WHERE
+                    systagparentid = 2
+                    AND systagtype = ${input.name.value.locale}
+            ),
+
+            master AS (
+                SELECT languagemasterid AS id
+                FROM public.languagemaster
+                WHERE languagemasterid IN (
+                    SELECT worktemplatenameid
+                    FROM public.worktemplate
+                    WHERE id = ${id}
+                )
+            ),
+
+            trans AS (
+                UPDATE public.languagetranslations
+                SET
+                    languagetranslationvalue = inputs.name,
+                    languagetranslationmodifieddate = now()
+                FROM inputs, master
+                WHERE
+                    languagetranslationmasterid = master.id
+                    AND languagetranslationtypeid = inputs.locale
+                    AND languagetranslationvalue IS DISTINCT FROM inputs.name
             )
-        )
 
         UPDATE public.languagemaster
         SET
-            languagemastersource = inputs.value,
-            languagemastersourcelanguagetypeid = locale.systagid,
+            languagemastersource = inputs.name,
+            languagemastersourcelanguagetypeid = inputs.locale,
             languagemasterstatus = 'NEEDS_COMPLETE_RETRANSLATION',
             languagemastermodifieddate = now()
-        FROM inputs, public.systag AS locale
+        FROM inputs, master
         WHERE
-            languagemasterid = (
-                SELECT worktemplatenameid
-                FROM public.worktemplate
-                WHERE id = ${id}
-            )
-            AND (locale.systagparentid, locale.systagtype) = (2, inputs.locale)
-            AND (
-                languagemastersource != inputs.value
-                OR
-                languagemastersourcelanguagetypeid != locale.systagid
-            )
+            languagemasterid = master.id
+            AND (languagemastersource, languagemastersourcelanguagetypeid) IS DISTINCT FROM (inputs.name, inputs.locale)
       `,
       input.sop
         ? tx`
