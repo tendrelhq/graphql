56b2c1c78b2571770f746336e95fb38cd29cfbc8
Will Ruggiano <wmruggiano@gmail.com>
2024-11-07 11:42:34 -0800

    fix: Duration scalar

--- schema/system/scalars/index.ts
+++ schema/system/scalars/index.ts
@@ -1,16 +1,18 @@
 import type { GraphQLScalarType } from "graphql";
 import {
-  GraphQLDuration,
+  // GraphQLDuration,
   GraphQLLocale,
   GraphQLTimeZone,
   GraphQLURL,
 } from "graphql-scalars";
 import { CronExpression } from "../resolvers/CronExpression";
 import { Entity } from "../resolvers/Entity";
+import { Duration } from "../resolvers/Duration";
 
 export const resolvers: Record<string, GraphQLScalarType> = {
   CronExpression: CronExpression,
-  Duration: GraphQLDuration,
+  // Duration: GraphQLDuration,
+  Duration: Duration,
   Entity: Entity,
   Locale: GraphQLLocale,
   TimeZone: GraphQLTimeZone,
@@ -22,8 +24,10 @@ export {
   CronExpression as CronExpressionResolver,
   Entity,
   Entity as EntityResolver,
-  GraphQLDuration as Duration,
-  GraphQLDuration as DurationResolver,
+  Duration,
+  Duration as DurationResolver,
+  // GraphQLDuration as Duration,
+  // GraphQLDuration as DurationResolver,
   GraphQLLocale as Locale,
   GraphQLLocale as LocaleResolver,
   GraphQLTimeZone as TimeZone,
@@ -34,7 +38,8 @@ export {
 
 export const config = {
   CronExpression: CronExpression.extensions.codegenScalarType,
-  Duration: GraphQLDuration.extensions.codegenScalarType,
+  // Duration: GraphQLDuration.extensions.codegenScalarType,
+  Duration: Duration.extensions.codegenScalarType,
   Entity: Entity.extensions.codegenScalarType,
   Locale: GraphQLLocale.extensions.codegenScalarType,
   TimeZone: GraphQLTimeZone.extensions.codegenScalarType,
