fa05fd7b7efab0602e22f9792de6cc389c0698b9
Will Ruggiano <wmruggiano@gmail.com>
2025-03-14 15:42:53 -0700

    fix(sql): permissions

--- sql/deploy/jwt.sql
+++ sql/deploy/jwt.sql
@@ -3,9 +3,22 @@
 BEGIN;
 
 CREATE SCHEMA crypto;
-CREATE EXTENSION pgcrypto SCHEMA crypto;
-
 CREATE SCHEMA jwt;
+
+-- Make sure we do this *before* we CREATE EXTENSION below!
+do $$
+begin
+  if exists (select 1 from pg_roles where rolname = 'graphql') then
+    revoke all on schema crypto from graphql;
+    revoke all on schema jwt from graphql;
+    grant usage on schema crypto to graphql;
+    grant usage on schema jwt to graphql;
+    alter default privileges in schema crypto grant execute on routines to graphql;
+    alter default privileges in schema jwt grant execute on routines to graphql;
+  end if;
+end $$;
+
+CREATE EXTENSION pgcrypto SCHEMA crypto;
 
 CREATE OR REPLACE FUNCTION jwt.base64_encode(data bytea) RETURNS text LANGUAGE sql AS $$
     SELECT translate(encode(data, 'base64'), E'+/=\n', '-_');
