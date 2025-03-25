7370bc1e672074510de30d56d006787287025b3c
Will Ruggiano <wmruggiano@gmail.com>
2025-03-25 13:31:58 -0700

    feat: deterministic runtime environment

---
+++ nix/ci.nix
@@ -0,0 +1,89 @@
+{inputs, ...}: {
+  perSystem = {
+    config,
+    self',
+    pkgs,
+    lib,
+    ...
+  }: {
+    devShells = {
+      ci = pkgs.mkShellNoCC {
+        name = "tendrelhq/graphql-ci";
+        inputsFrom = [
+          config.devShells.default
+        ];
+        TREEFMT = "treefmt";
+      };
+    };
+
+    packages = let
+      pkg = lib.importJSON ../package.json;
+      pname = lib.replaceStrings ["@"] [""] pkg.name;
+    in {
+      default = pkgs.stdenv.mkDerivation {
+        inherit pname;
+        inherit (pkg) version;
+        src = pkgs.nix-gitignore.gitignoreSource [../.dockerignore] ../.;
+
+        nativeBuildInputs = with pkgs; [bun nodejs config.packages.node_modules];
+
+        dontFixup = true; # patchShebangs produces illegal path references in FODs
+
+        configurePhase = ''
+          runHook preConfigure
+          cp -a ${config.packages.node_modules}/node_modules ./node_modules
+          chmod -R u+rw node_modules
+          chmod -R u+x node_modules/.bin
+          patchShebangs node_modules
+          export PATH="$PWD/node_modules/.bin:$PATH"
+          bun generate
+          runHook postConfigure
+        '';
+
+        buildPhase = ''
+          runHook preBuild
+          bun build ./bin/app.ts --compile --minify --sourcemap
+          runHook postBuild
+        '';
+
+        installPhase = ''
+          runHook preInstall
+          mkdir -p $out/bin
+          cp app $out/bin/entrypoint
+          runHook postInstall
+        '';
+
+        meta.mainProgram = "entrypoint"; # so you can `nix run`
+      };
+
+      node_modules = pkgs.stdenv.mkDerivation {
+        pname = "${pname}-deps";
+        inherit (pkg) version;
+        src = pkgs.nix-gitignore.gitignoreSource [../.dockerignore] ../.;
+
+        nativeBuildInputs = [pkgs.bun];
+
+        dontConfigure = true;
+        dontFixup = true; # patchShebangs produces illegal path references in FODs
+
+        buildPhase = ''
+          runHook preBuild
+          export HOME=$TMPDIR
+          bun install --no-cache --no-progress --frozen-lockfile --ignore-scripts
+          runHook postBuild
+        '';
+
+        installPhase = ''
+          runHook preInstall
+          mkdir -p $out/node_modules
+          mv node_modules $out/
+          runHook postInstall
+        '';
+
+        outputHash = "sha256-enI7GLK5PvFrLhhtI18ol6Nx1LoQC1/JfO6ME7/TEx0=";
+        outputHashAlgo = "sha256";
+        outputHashMode = "recursive";
+      };
+    };
+  };
+}
